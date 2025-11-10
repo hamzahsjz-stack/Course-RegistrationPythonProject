ea1b9e4eb99dcea1a8c14186f73af39cee3edf20
hamzahsjz-stack <hamzahsjz@gmail.com>
2025-11-10 12:45:46 +0300

    Create course.py

---
+++ course.py
@@ -0,0 +1,34 @@
+# TODO:
+#Purpose:
+#Let administrators define and manage all course data.
+
+#Features:
+
+#Add/edit/delete courses:
+
+#Course code, name, credits, lecture hours, lab hours, prerequisites, max capacity.
+
+#Link courses to specific programs and levels (e.g., Computer L3 S1).
+
+#Validate all inputs:
+
+#No duplicate course codes.
+
+#Credit hours must be positive.
+
+#Prerequisites must exist in system.
+
+#Bulk import via CSV/Excel.
+
+#Stored in Database Tables:
+
+#courses
+
+#program_plans
+
+#prerequisites
+
+#Errors displayed as messages:
+
+#"Course code COE310 already exists"
+#"Prerequisite COE200 not found"
