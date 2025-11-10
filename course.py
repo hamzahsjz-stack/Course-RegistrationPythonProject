47b89d94601fc8134ca68e74b243c75b63dbf97f
hamzahsjz-stack <hamzahsjz@gmail.com>
2025-11-10 13:53:27 +0300

    Update course.py

--- course.py
+++ course.py
@@ -32,3 +32,21 @@
 
 #"Course code COE310 already exists"
 #"Prerequisite COE200 not found"
+class Course:
+    def __init__(self,code,name,credits,hours,lecture_hours,lab_hours,max_capacity,prerequisites=None):
+        self.code = code
+        self.name = name
+        self.credits = credits
+        self.hours = hours
+        self.lecture_hours = lecture_hours
+        self.lab_hours = lab_hours
+        self.max_capacity = max_capacity
+        self.prerequisites = prerequisites or []
+        self.enrolled_Students = 0
+    def isFull(self):
+        #check if the course has the max enrolled students if true return true if false return false
+        return self.enrolled_Students >= self.max_capacity
+    def CheckPrerequisites(self, student_transcript):
+        # """Return True if all prerequisites are met.""" 
+        return all(p in student_transcript for p in self.prerequisites)
+    
