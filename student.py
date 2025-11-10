343531cbd0863f11a60fbe55c144e74209449778
hamzahsjz-stack <hamzahsjz@gmail.com>
2025-11-10 13:53:56 +0300

    Implement Student class with transcript management
    Added Student class with initialization and transcript methods.

--- student.py
+++ student.py
@@ -22,3 +22,24 @@
       
       #"Student ID already exists"
       #"Please select a valid program"
+class Student:
+    
+    def __init__(self, student_id, name, email, program, level, transcript=None):
+        self.student_id = student_id
+        self.name = name
+        self.email = email
+        self.program = program
+        self.level = level
+        self.CreditHours = 0
+        self.transcript = transcript or []
+    
+    
+    def getCreditHours(self,All_Courses):
+        total = 0
+        for course_code in self.transcript:
+            if course_code in All_Courses:
+                total += All_Courses[course_code].credits
+        return total
+    def add_to_transcript(self,course_code):
+        if course_code not in self.transcript:
+            self.transcript.append(course_code)
