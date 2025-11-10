e1bf03d36bf7dd58597d342eb70d34743ae458d6
hamzahsjz-stack <hamzahsjz@gmail.com>
2025-11-10 12:41:23 +0300

    Enhance TODO comments in auth.py
    Added detailed purpose and features for the authentication system.

--- auth.py
+++ auth.py
@@ -1,1 +1,27 @@
-# TODO: Create username password system and authorization
+# TODO: Purpose: Purpose:
+      #Secure login system with different roles.
+      
+      #Features:
+      
+      #Two roles: Student and Admin
+      
+      #Login with email/ID + password.
+      
+      #Passwords encrypted with bcrypt.
+      
+      #Redirect users to appropriate dashboards:
+      
+      #Students → registration panel.
+      
+      #Admins → course management panel.
+      
+      #Password recovery (email reset).
+      
+      #Database Table:
+      
+      #users
+      
+      #Example errors:
+      
+      #"Invalid ID or password"
+      #"Email already registered" 
